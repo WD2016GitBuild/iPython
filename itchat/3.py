f806e800d3de377ce7da04213268564bae252235
2016GitBuild <wld74769@163.com>
2018-03-30 14:55:27 +0800

    指定发送给某个人

--- itchat/3.py
+++ itchat/3.py
@@ -18,13 +18,35 @@ rooms = itchat.get_chatrooms(update=True)
 rooms = itchat.search_chatrooms('Design')[0]['UserName']
 
 companyName = sys.argv[1]
+toWho = sys.argv[2]
+
+def listCount(name):
+    account = itchat.get_friends()
+    for x in account:
+        # print(x['NickName'])
+        # print(x['RemarkName'])
+        if(x['RemarkName'] == name):
+            return x['UserName']
+            break
+
+def listRoom():
+    rooms = itchat.get_chatrooms(update=True)
+    rooms = itchat.search_chatrooms('幸福一家人')
+    print(len(rooms))
+    for x in rooms:
+        print(x['NickName'])
+
 print(companyName)
 print('开始质检...')
+print('质检结果后将结果发送给微信-' + toWho)
+room = listCount(toWho)
+print('查找到UserName：' + room)
 
 def handle(code,name,type,status):
     if status == '制作中':
         print('订单号：' + code + '，客户名称：' + name + '，质检被打回...')
         itchat.send('订单号：' + code + '，客户名称：' + name + '，质检被打回...',toUserName=mySelf)
+        itchat.send('订单号：' + code + '，客户名称：' + name + '，质检被打回...',toUserName=rooms)
     elif status == '待发布':
         print('订单号：' + code + '，客户名称：' + name + '，质检通过了!')
         itchat.send('订单号：' + code + '，客户名称：' + name + '，质检通过了!',toUserName=mySelf)
@@ -33,12 +55,13 @@ def handle(code,name,type,status):
 search(companyName, handle)
 # itchat.send('哈哈',toUserName=rooms)
 
+
+
+
 def main():
-	rooms = itchat.get_chatrooms(update=True)
-	rooms = itchat.search_chatrooms('幸福一家人')
-	print(len(rooms))
-	for x in rooms:
-		print(x)
+    listRoom()
+    listCount()
+
 
 if __name__ == '__main__':
 	# main()
